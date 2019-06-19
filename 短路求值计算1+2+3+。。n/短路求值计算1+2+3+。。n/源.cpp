a005edf0e55bf00c43b6a6e07c887b6ffd544b13
zh593245631 <593245631@qq.com>
2019-06-19 12:47:55 +0800

    类

--- 短路求值计算1+2+3+。。n/短路求值计算1+2+3+。。n/源.cpp
+++ 短路求值计算1+2+3+。。n/短路求值计算1+2+3+。。n/源.cpp
@@ -1,8 +1,34 @@
-class Solution {
+//class Solution {
+//public:
+//	int Sum_Solution(int n) {
+//		int ans = n;
+//		ans && (ans += Sum_Solution(n - 1));
+//		return ans;
+//	}
+//};
+
+class Solution
+{
 public:
-	int Sum_Solution(int n) {
-		int ans = n;
-		ans && (ans += Sum_Solution(n - 1));
-		return ans;
+	Solution()
+	{
+		++N;
+		Sum += N;
 	}
+	int Sum_Solution(int n)
+	{
+		N = 0;
+		Sum = 0;
+		Solution* a = new Solution[n];
+		delete[]a;
+		a = nullptr;
+
+		return Sum;
+	}
+private:
+	static int N;
+	static int Sum;
 };
+
+int Solution::N = 0;
+int Solution::Sum = 0;
