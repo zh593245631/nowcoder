9c792751c07437e8e96e5265b7271ad08e971e08
zh593245631 <593245631@qq.com>
2019-06-27 21:55:49 +0800

    复习，添加注释

--- 计算字符串的距离/计算字符串的距离/源.cpp
+++ 计算字符串的距离/计算字符串的距离/源.cpp
@@ -1,35 +1,37 @@
 #include<iostream>
-#include<vector>
 #include<string>
-#include<algorithm>
-
+#include<vector>
 using namespace std;
-
+int Len(string& a, string& b)
+{
+	int n = a.size();
+	int m = b.size();
+	vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+	dp[0][0] = 0;
+	for (int i = 1; i <= n; ++i)dp[i][0] = i;
+	for (int j = 1; j <= m; ++j)dp[0][j] = j;
+	for (int i = 1; i <= n; ++i)
+	{
+		for (int j = 1; j <= m; ++j)
+		{
+			//²åÈë/É¾³ý
+			int one = dp[i - 1][j] + 1;//a²åb
+			int two = dp[i][j - 1] + 1;//b²åa
+			//Ìæ»»
+			int tree = dp[i - 1][j - 1];
+			if (a[i - 1] != b[j - 1])++tree;
+			dp[i][j] = min(min(one, two), tree);
+		}
+	}
+	return dp[n][m];
+}
 int main()
 {
 	string str1, str2;
 	while (getline(cin, str1))
 	{
 		getline(cin, str2);
-		int m = str1.size();
-		int n = str2.size();
-		vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
-		dp[0][0] = 0;
-		for (int i = 1; i < m + 1; ++i)dp[i][0] = i;
-		for (int j = 1; j < n + 1; ++j)dp[0][j] = j;
-		for (int i = 1; i < m + 1; ++i)
-		{
-			for (int j = 1; j < n + 1; ++j)
-			{
-				int s1 = dp[i - 1][j] + 1;
-				int s2 = dp[i][j - 1] + 1;
-				int s3 = dp[i - 1][j - 1];
-				if (str1[i - 1] != str2[j - 1])++s3;
-				dp[i][j] = min(s1, s2);
-				dp[i][j] = min(dp[i][j], s3);
-			}
-		}
-		cout << dp[m][n] << endl;
+		cout << Len(str1, str2) << endl;
 	}
 	return 0;
 }
